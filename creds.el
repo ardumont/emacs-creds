fcbc9a106a1de16a9d8300474d55a70ed26dc37d
Antoine R. Dumont <eniotna.t@gmail.com>
2014-05-04 22:57:33 +0200

    Update to version 0.0.5.1

--- creds.el
+++ creds.el
@@ -5,7 +5,7 @@
 
 ;; Author: Antoine R. Dumont <eniotna.t AT gmail.com>
 ;; Maintainer: Antoine R. Dumont <eniotna.t AT gmail.com>
-;; Version: 0.0.5
+;; Version: 0.0.5.1
 ;; Package-Requires: ((dash "2.5.0"))
 ;; Keywords: credentials
 ;; URL: https://github.com/ardumont/emacs-creds
