681c6b4511386b69e97ea93f032490643c171c9e
Antoine R. Dumont <eniotna.t@gmail.com>
2014-05-07 21:04:55 +0200

    - [X] Fix bug regarding passwords entry with blank characters

--- creds.el
+++ creds.el
@@ -61,11 +61,35 @@
 (require 'dash)
 (require 's)
 
-(defun creds/read-lines (filepath)
-  "Return a list of lines from a file FILEPATH."
+(defvar *creds/protection-string-against-blank-char* "@#$~!!~$#@" "A string to replace blank space.")
+
+(defun creds/--protect-blank-spaced-words (s)
+  "Protect the string S by removing blank space and \"."
+  (->> s
+    (replace-regexp-in-string " " *creds/protection-string-against-blank-char*)
+    (replace-regexp-in-string "\"" "")))
+
+(defun creds/--unprotect-blank-spaced-words (s)
+  "Unprotectd the string S by replacing the protected characters by blank space."
+  (replace-regexp-in-string *creds/protection-string-against-blank-char* " " s))
+
+(defun creds/--read-and-protect-content-file (filepath)
+  "Given a file FILEPATH, return the contents of such file with potential blank spaced word protected."
   (with-temp-buffer
     (insert-file-contents filepath)
-    (--map (split-string it "[ ]+") (split-string (buffer-string) "\n" t))))
+    (goto-char (point-min))
+    (while (re-search-forward "\".*\"" nil t)
+      (let ((string-to-replace (-> (match-string-no-properties 0) creds/--protect-blank-spaced-words)))
+        (replace-match string-to-replace nil t)))
+    (buffer-string)))
+
+(defun creds/read-lines (filepath)
+  "Return a list of lines from a file FILEPATH."
+  (->> filepath
+    creds/--read-and-protect-content-file
+    s-lines
+    (--map (let ((protected-words (s-split " " it)))
+             (mapcar #'creds/--unprotect-blank-spaced-words protected-words)))))
 
 (defun creds/get-with (data key-value-pairs)
   "Return the DATA list for the list KEY-VALUE-PAIRS of associations."
